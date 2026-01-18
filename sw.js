b15b4e1cda824bd40b042fc6565bdf76f46ac402
Jagmohan1313 <ysf5n6czy5@privaterelay.appleid.com>
2026-01-19 03:32:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'pbx-0008-v1';
+const ASSETS = [
+  'jagmoHhhHan.html',
+  'manifest.json',
+  'icon-180.png',
+  'icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
